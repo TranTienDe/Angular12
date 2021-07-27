61aafea46153943b2c9bbb95d30875a66547e1ce
TranTienDe <sakurahonni@gmail.com>
2021-07-27 22:01:19 +0700

    [ngClass] *ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,12 @@ export class HomeComponent implements OnInit {
   public name = 'Huy Nguyễn';
   public age = 25;
   public traiCayList = ['Táo', 'Nho', 'Cam', 'Bưởi'];
+  public traiCayList2 = [
+    { ten: 'Táo', gia: 12.590, haGia: true },
+    { ten: 'Nho', gia: 3.090, haGia: false },
+    { ten: 'Bười', gia: -13.09, haGia: false },
+    { ten: 'Cam', gia: 42.02, haGia: true }
+  ];
 
   constructor() { }
 

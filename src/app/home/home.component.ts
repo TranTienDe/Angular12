1368d8157f49fe83724d661e9a02bacc2b3107d0
TranTienDe <sakurahonni@gmail.com>
2021-07-27 18:17:40 +0700

    *ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public name = 'Huy Nguyễn';
+  public name = 'Minh Khánh';
+  public age = 35;
 
   constructor() { }
 

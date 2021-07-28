aa49920d6eee5d5f15938658e5eef88e7079c219
TranTienDe <sakurahonni@gmail.com>
2021-07-28 15:41:47 +0700

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'home', component: HomeComponent },
-  {path: '**', component: PageNotFoundComponent}
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
